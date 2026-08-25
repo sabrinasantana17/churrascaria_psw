05203a96f1b506e5544088f18a485570cc054b5d
Vinyii <146475782+Vinyii@users.noreply.github.com>
2026-08-25 14:09:47 -0300

    q

--- item/models.py
+++ item/models.py
@@ -3,12 +3,19 @@ from django.core.validators import MinValueValidator
 
 
 class Item(models.Model):
+
+    CATEGORIA_CHOICES = [
+        ('CARNE', 'Carne'),
+        ('ACOMPANHAMENTO', 'Acompanhamento'),
+        ('BEBIDA', 'Bebida'),
+        ('SOBREMESA', 'Sobremesa'),
+    ]
+
     nome = models.CharField(max_length=100)
     descricao = models.TextField(blank=True)
     preco = models.DecimalField(max_digits=8, decimal_places=2, validators=[MinValueValidator(0)])
-    categoria = models.CharField(max_length=50)
+    categoria = models.CharField(max_length=20, choices=CATEGORIA_CHOICES)
     disponivel = models.BooleanField(default=True)
-
-    
+   
     def __str__(self):
         return self.nome
